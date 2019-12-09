38b4e152d926ec1ac05fe01ed46b7bd4cb97e157
KalluruS <>
2019-12-09 21:36:58 +0530

    --- Auto-Git Commit ---

---
+++ 02-variables/sample.tf
@@ -0,0 +1,7 @@
+// Declare a string
+
+variable "simple-string" {
+  default = "SIMPLE"
+}
+
+// Note: If you give the value in double quotes then terraform automatically considers that as a string.
