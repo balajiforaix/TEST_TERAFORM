8c104f1d03e61114ccc6090dff37ff21e0c7bdcd
KalluruS <>
2019-12-09 21:39:19 +0530

    --- Auto-Git Commit ---

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -8,5 +8,12 @@ variable "simple-string" {
 // Note: If you give the value in double quotes then terraform automatically considers that as a string.
 
 variable "simple-number" {
-  
+  type = number // No need of this if you give the default value is a number and given with out quotes
+  default = 100
 }
+
+variable "simple-boolean" {
+  type = bool  // No need of this if you give the default value is a boolean (true / false) and given with out quotes
+  default = true
+}
+
