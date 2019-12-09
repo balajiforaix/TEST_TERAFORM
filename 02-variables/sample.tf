8c34800a14811ec2c9ed86c3e0a4b9d1734289af
KalluruS <>
2019-12-09 21:37:49 +0530

    --- Auto-Git Commit ---

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -1,8 +1,12 @@
 // Declare a string
 
 variable "simple-string" {
-  type = "string" // No need of this if you give the value in double quot
+  type    = "string" // No need of this if you give the value in double quotes
   default = "SIMPLE"
 }
 
 // Note: If you give the value in double quotes then terraform automatically considers that as a string.
+
+variable "simple-number" {
+  
+}
