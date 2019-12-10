8083b7aba041b487b5b3d53617e4dd9d9287dfe6
KalluruS <>
2019-12-10 21:14:00 +0530

    --- Auto-Git Commit ---

--- PROJECT/studentapp/stage-01/ec2.tf
+++ PROJECT/studentapp/stage-01/ec2.tf
@@ -9,7 +9,7 @@ variable "ami_id" {
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0aea1c5e2dcfc9837"]
+  vpc_security_group_ids = ["sg-0aea1c5e2dcfc9837", "sg-0e1c03ec34479caf5"]
 
   provisioner "remote-exec" {
     connection {
