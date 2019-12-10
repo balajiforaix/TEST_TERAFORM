d13d6380fb46d807735cb27d72ce53dbb41d56c7
KalluruS <>
2019-12-10 20:50:16 +0530

    --- Auto-Git Commit ---

--- PROJECT/studentapp/stage-01/ec2.tf
+++ PROJECT/studentapp/stage-01/ec2.tf
@@ -9,5 +9,5 @@ variable "ami_id" {
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = "t2.micro"
-  vpc_security_group_ids = [""]
+  vpc_security_group_ids = ["sg-0aea1c5e2dcfc9837"]
 }
